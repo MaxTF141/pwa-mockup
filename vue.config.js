8d98663238951edb14bd8002fe9ec976cfc3316b
MaxTF141 <isaacsmaxwill22@gmail.com>
2023-06-12 13:10:31 +0200

    test

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  pwa: {
+    name: 'Your App Name', // Replace with your app's name
+    themeColor: '#ffffff', // Replace with your preferred theme color
+    msTileColor: '#ffffff', // Replace with your preferred tile color
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+    workboxOptions: {
+      exclude: [/\.map$/, /_redirects/], // Exclude unnecessary files from the service worker
+    },
+  },
+};
+
