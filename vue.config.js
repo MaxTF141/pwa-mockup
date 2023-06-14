9580ca43912d1df24631e9236e48cd2e2235f0bc
MaxTF141 <isaacsmaxwill22@gmail.com>
2023-06-14 20:42:55 +0200

    home

--- vue.config.js
+++ vue.config.js
@@ -2,29 +2,28 @@ module.exports = {
   pwa: {
     name: 'Spotify',
     themeColor: '#1db954',
-    msTileColor: '#000000',
+    msTileColor: '#1db954',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     workboxOptions: {
       // Configure options for the service worker
     },
     iconPaths: {
-      favicon32: 'src/assets/icon3@2x.png',
-      favicon16: 'src/assets/icon3@2x.png',
-      appleTouchIcon: 'src/assets/icon3@2x.png',
-      maskIcon: 'src/assets/icon3@2x.png',
-      msTileImage: 'src/assets/icon3@2x.png',
+      favicon32: 'src/assets/Spotify_Icon_RGB_Green.png',
+      favicon16: 'src/assets/Spotify_Icon_RGB_Green.png',
+      appleTouchIcon: 'src/assets/Spotify_Icon_RGB_Green.png',
+      maskIcon: 'src/assets/Spotify_Icon_RGB_Green.png',
+      msTileImage: 'src/assets/Spotify_Icon_RGB_Green.png',
     },
     manifestOptions: {
-      background_color: '##191414',
+      background_color: '#191414',
       icons: [
         {
-          src: 'src/assets/logos_spotify-crop-removebg-preview.png',
-          sizes: '749x333',
+          src: 'src/assets/Spotify_Logo_RGB_Green.png',
+          sizes: '2362x709',
           type: 'image/png',
         },
       ],
-      name: 'Spotify', // Add or update this line to set the name to an empty string
     },
   },
 };
